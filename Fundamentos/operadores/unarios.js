c3b7de0bef7ce657be8829a5015a099c19c0acf6
newryan <ildson.leno@outlook.com>
2021-10-30 10:06:24 -0300

    continuidade do curso

---
+++ Fundamentos/operadores/unarios.js
@@ -0,0 +1,11 @@
+let a = 1; //operando incremento (infix)
+
+a = a + 1; //operando incremento (infix)
+a += 1; //operando incremento (infix)
+a++; //operando incremento (postfix)
+++a; //operando incremento (prefix)
+
+a--; //operando incremento (postfix)
+--a; //operando incremento (prefix)
+
+console.log(a);
