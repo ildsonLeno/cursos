48c9fb4a4f88828e320b921b665fb69a3294b901
Ildson Leno <ildson.leno@outlook.com>
2021-12-21 15:39:35 -0300

    21_12

---
+++ Fundamentos/funcoes/desafio_funcao_1.js
@@ -0,0 +1,14 @@
+
+function Soma(a, b) {
+    console.log(a + b);
+}
+
+function Subtracao(c, d) {
+    console.log(c - d);
+}
+
+Soma(33, 36);
+Soma(18, 22);
+
+Subtracao(52, 21);
+Subtracao(25, 15);
