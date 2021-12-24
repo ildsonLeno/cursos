845998dd86f53ebd86abeab2045812c11f1a086e
Ildson Leno <ildson.leno@outlook.com>
2021-12-24 17:37:19 -0300

    24_12

---
+++ Fundamentos/funcoes/funcao_como_var_1.js
@@ -0,0 +1,11 @@
+// armazenar função dentro de uma variavel ou constante.
+// não dar nome a função.
+
+const x = 123;
+let y = 321;
+
+const somar = function (a, b) {
+    return a + b;
+}
+
+console.log(somar(x, y));
